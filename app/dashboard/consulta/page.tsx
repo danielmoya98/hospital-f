2e0f9020d28ce80d81e6de25c2104df342c77742
Alienware <moycarlos09@gmail.com>
2024-12-09 10:39:17 -0400

    intial

--- app/dashboard/consulta/page.tsx
+++ app/dashboard/consulta/page.tsx
@@ -1,6 +1,6 @@
 import { ConsultationForm } from "@/components/consultation-form"
-import {PatientProvider} from "@/context/PatientContext";
-import {EmployeeProvider} from "@/context/EmployeeContext";
+
+
 
 export default function ConsultationPage() {
     return <ConsultationForm />
